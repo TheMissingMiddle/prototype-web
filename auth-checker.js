290e625c8f6f8bc9b1ad4f5ce20cb27bf4b481b3
Alex Fang <frjalex@temple.edu>
2017-05-31 19:27:31 -0400

    Auth

---
+++ auth-checker.js
@@ -0,0 +1,13 @@
+function authCheck(isPublic, jumpToPage) {
+	 // check if already authenticated
+ if(  getCookie(config.cookieName) != undefined ) { 
+ 	if(isPublic) {
+ 		window.location.href = jumpToPage;
+ 	}
+ } else {
+ 	// go to login page
+ 	if(!isPublic) {
+ 		window.location.href = jumpToPage;
+ 	} 
+ }
+}
---
+++ settings.js
@@ -0,0 +1,9 @@
+ var config = {
+ 	endpoint: 'http://localhost:3000/',
+    cookieName: 'TMMJwtToken',
+    cookieExpirationDays: 7,
+    loginPage: 'index.html',
+    mainPage: 'main.html',
+    chatPage: 'chat.html',
+    contactsPage: 'contacts.html'
+ }
