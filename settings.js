3b98e8224e2d76ce2e505cb61370eb6c9c683f7f
Alex Fang <frjalex@temple.edu>
2017-06-02 22:59:05 -0400

    Complete login/register logic and testing

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
  var config = {
- 	endpoint: 'http://localhost:3000/',
+ 	endpoint: 'http://localhost:3000',
     cookieName: 'TMMJwtToken',
     cookieExpirationDays: 7,
     loginPage: 'index.html',
